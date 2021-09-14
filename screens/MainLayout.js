292cd2a11f73dbe72ec6e47bc81649994fc9f56c
Greig Paais <greigpaais@gmail.com>
2021-09-14 20:21:37 +0900

    Fix Cycles on dummyData

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,11 +1,44 @@
 import React from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Image,
+    FlatList
 } from 'react-native';
-import Animated from "react-native-reanimated"
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withTiming
+} from "react-native-reanimated"
+import { connect } from 'react-redux';
+import { setSelectedTab } from '../stores/tab/tabActions';
 
-const MainLayout = ({ drawerAnimationStyle }) => {
+import { Home,
+    Search,
+    CartTab,
+    Favourite, 
+    Notification 
+} from "../screens";
+import { Header } from "../components"
+
+import {
+    COLORS,
+    FONTS,
+    SIZES,
+    icons,
+    constants,
+    dummyData
+} from "../constants";
+
+const MainLayout = ({ drawerAnimationStyle, navigation,
+selectedTab, setSelectedTab }) => {
+
+    React.useEffect(() => {
+        setSelectedTab(constants.screens.home)
+    }, [])
+
     return (
         <Animated.View
             style={{
@@ -16,9 +49,42 @@ const MainLayout = ({ drawerAnimationStyle }) => {
                 ...drawerAnimationStyle
             }}
         >
-            <Text>MainLayout</Text>
+            {/* Header */}
+            <Header
+                containerStyle={{
+                    height: 50,
+                    paddingHorizontal: SIZES.padding,
+                    marginTop: 40,
+                    alignItems: 'center'
+                }}
+                title={selectedTab.toUpperCase()}
+            />
+            {/* Content */}
+            <View
+                style={{
+                    flex: 1
+                }}
+            >
+                <Text>MainLayout</Text>
+            </View>
+
+            {/* Footer */}
         </Animated.View>
     )
 }
 
-export default MainLayout;
+function mapStateToProps(state) {
+    return {
+        selectedTab: state.tabReducer.selectedTab
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setSelectedTab: (selectedTab) => { return dispatch
+        (setSelectedTab(selectedTab)) }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)
+(MainLayout)
