29ba77282b9a4bd9d8ef9d434764d823f425b19c
Greig Paais <greigpaais@gmail.com>
2021-09-15 13:07:37 +0900

    Adding Main Buttons on the bottom of App

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -12,6 +12,7 @@ import Animated, {
     useAnimatedStyle,
     withTiming
 } from "react-native-reanimated"
+import LinearGradient from 'react-native-linear-gradient';
 import { connect } from 'react-redux';
 import { setSelectedTab } from '../stores/tab/tabActions';
 
@@ -32,6 +33,55 @@ import {
     dummyData
 } from "../constants";
 
+const TabButton = ({ label, icon, isFocused, onPress}) => {
+    return (
+        <TouchableWithoutFeedback
+            onPress={onPress}
+        >
+            <Animated.View
+                style={{
+                    flex: 1,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Animated.View
+                    style={{
+                        flexDirection: 'row',
+                        width: "80%",
+                        height: 50,
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        borderRadius: 25
+                    }}
+                >
+                    <Image
+                        source={icon}
+                        style={{
+                            width: 20,
+                            height: 20,
+                            tintColor: COLORS.gray
+                        }}
+                    />
+
+                    {isFocused &&
+                        <Text
+                            numberOfLines={1}
+                            style={{
+                                marginLeft: SIZES.base,
+                                color: COLORS.gray,
+                                ...FONTS.h3
+                            }}
+                       >
+                           {label}
+                        </Text>
+                    }
+                </Animated.View>
+            </Animated.View>
+        </TouchableWithoutFeedback>
+    )
+}
+
 const MainLayout = ({ drawerAnimationStyle, navigation,
 selectedTab, setSelectedTab }) => {
 
@@ -43,9 +93,7 @@ selectedTab, setSelectedTab }) => {
         <Animated.View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: 'white',
+                backgroundColor: COLORS.white,
                 ...drawerAnimationStyle
             }}
         >
@@ -107,6 +155,88 @@ selectedTab, setSelectedTab }) => {
             </View>
 
             {/* Footer */}
+            <View
+                style={{
+                    height: 100,
+                    justifyContent: 'flex-end'
+                }}
+            >
+                {/* Shadow */}
+                <LinearGradient
+                    start={{x: 0, y: 0}}
+                    end={{x: 0, y: 4}}
+                    colors={[
+                        COLORS.transparent,
+                        COLORS.lightGray1
+                    ]}
+                    style={{
+                        position: 'absolute',
+                        top: -20,
+                        left: 0,
+                        right: 0,
+                        height: 100,
+                        borderTopLeftRadius: 15,
+                        borderTopRightRadius: 15
+                    }}
+                />
+
+                {/* Tabs */}
+                <View
+                    style={{
+                        flex: 1,
+                        flexDirection: 'row',
+                        paddingHorizontal: SIZES.radius,
+                        paddingBottom: 10,
+                        borderTopLeftRadius: 20,
+                        borderTopLeftRadius: 20,
+                        backgroundColor: COLORS.white
+                    }}
+                >
+                    <TabButton
+                        label={constants.screens.home}
+                        icon={icons.home}
+                        isFocused={selectedTab == constants.
+                        screens.home}
+                        onPress={() => setSelectedTab(constants.
+                        screens.home)}
+                    />
+                    
+                    <TabButton
+                        label={constants.screens.search}
+                        icon={icons.search}
+                        isFocused={selectedTab == constants.
+                        screens.search}
+                        onPress={() => setSelectedTab(constants.
+                        screens.search)}
+                    />
+
+<TabButton
+                        label={constants.screens.cart}
+                        icon={icons.cart}
+                        isFocused={selectedTab == constants.
+                        screens.cart}
+                        onPress={() => setSelectedTab(constants.
+                        screens.cart)}
+                    />
+                    <TabButton
+                        label={constants.screens.favourite}
+                        icon={icons.favourite}
+                        isFocused={selectedTab == constants.
+                        screens.favourite}
+                        onPress={() => setSelectedTab(constants.
+                        screens.favourite)}
+                    />
+
+<TabButton
+                        label={constants.screens.notification}
+                        icon={icons.notification}
+                        isFocused={selectedTab == constants.
+                        screens.notification}
+                        onPress={() => setSelectedTab(constants.
+                        screens.notification)}
+                    />
+                </View>
+            </View>
         </Animated.View>
     )
 }
