0296887e8b7968270d8e7e7935a6cbea959b3eac
Greig Paais <greigpaais@gmail.com>
2021-09-18 12:06:46 +0900

    Adding Reccomended Food Card

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -10,10 +10,44 @@ import {
 import { HorizontalFoodCard } from '../../components';
 import { FONTS, SIZES, COLORS, icons, dummyData } from '../../constants';
 
+const  Section =({ title, onPress, children}) => {
+    return (
+        <View>
+            {/* Header */}
+            <View
+             style={{
+                 flexDirection: 'row',
+                 marginHorizontal: SIZES.padding,
+                 marginTop: 30,
+                 marginBottom: 20
+             }}
+            >
+
+                <Text style={{ flex: 1, ...FONTS.h3 }}>
+                    {title}
+                </Text>
+
+                <TouchableOpacity
+                    onPress={onPress}
+                >
+                    <Text style={{ color: COLORS.primary, ...FONTS.body3 }}>
+                        Show All
+                    </Text>
+                </TouchableOpacity>
+
+            </View>
+
+            {/* Content */}
+            {children}
+        </View>
+    )
+}
+
 const Home = () => {
 
     const [selectedCategoryId, setSelectedCategoryId] = React.useState(1)
     const [selectedMenuType, setSelectedMenuType] = React.useState(1)
+    const [recommends, setRecommends] = React.useState([])
     const [menuList, setMenuList] = React.useState([])
 
     React,useEffect(() => {
@@ -23,9 +57,17 @@ const Home = () => {
     // Handler
 
     function handleChangeCategory(categoryId, menuTypeId) {
+        // Retrieve the recommended menu
+        let selectedRecommend = dummyData.menu.find(a => a.name == "Recommended")
+
         // Find the menu based on the menuTypeId
         let selectedMenu = dummyData.menu.find(a => a.id == 
         menuTypeId)
+
+        // Set the recommended menu based on the categoryId
+        setRecommends(selectedRecommend?.list.filter(a=> a.
+        categories.includes(categoryId)))
+
         // Set the menu based on the categoryId
         setMenuList(selectedMenu?.list.filter(a => a.categories.
             includes(categoryId)))
@@ -103,6 +145,11 @@ const Home = () => {
                             marginLeft: SIZES.padding,
                             marginRight: index == dummyData.menu.length - 1 ? SIZES.padding : 0
                         }}
+                        onPress={() => {
+                            setSelectedMenuType(item.id)
+                            handleChangeCategory
+                            (selectedCategoryId, item.id)
+                        }}
                     >
                         <Text
                             style={{
@@ -115,6 +162,42 @@ const Home = () => {
                     </TouchableOpacity>
                 )}
             />
+        )
+    }
+
+    function renderRecommendedSection() {
+        return (
+            <Section
+                title="Recommended"
+                onPress={() => console.log("Show all recommended")}
+            >
+                <FlatList
+                    data={recommends}
+                    keyExtractor={item => `${item.id}`}
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    renderItem={({item, index}) => (
+                        <HorizontalFoodCard
+                            containerStyle={{
+                                height: 180,
+                                width: SIZES.width * 0.85,
+                                marginLeft: index == 0 ? SIZES.padding: 18,
+                                marginRight: index == recommends.length - 1 ? SIZES.padding : 0,
+                                paddingRight: SIZES.radius,
+                                alignItems: 'center'
+                            }}
+                            imageStyle={{
+                                marginTop: 35,
+                                height: 150,
+                                width: 150
+                            }}
+                            item={item}
+                            onPress={() => console.log
+                            ("HorizontalFoodCard")}
+                        />
+                    )}
+                />
+            </Section>
         )
     }
 
@@ -134,6 +217,8 @@ const Home = () => {
                 showsVerticalScrollIndicator={false}
                 ListHeaderComponent={
                     <View>
+                        {/* Recommended */}
+                        {renderRecommendedSection()}
 
                         {/* Menu Type */}
                         {renderMenuTypes()}
